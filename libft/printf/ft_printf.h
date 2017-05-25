8051fbdb33ef7ebf90702bb06b68ba0d15ce2da4
Florent Bonnin <florent-bonnin@live.fr>
2017-05-26 01:06:43 +0200

    l

--- libft/printf/ft_printf.h
+++ libft/printf/ft_printf.h
@@ -18,6 +18,7 @@ typedef struct	s_printf
 	int						nb_chars_written;
 	char					buffer[PRINTF_BUFFER_SIZE];
 	int						i_buffer;
+
 	int						flag_minus;
 	int						flag_zero;
 	int						flag_sharp;
@@ -27,10 +28,13 @@ typedef struct	s_printf
 	int						precision;
 	int						size;
 	char					type;
+
 	char					c;
+
 	char					*str;
 	wchar_t					*wstr;
 	int						len_str;
+
 	unsigned long long int	ullint;
 	long long int			llint;
 	char					prefix[2];
@@ -45,42 +49,31 @@ typedef struct	s_printf
 int				ft_printf(const char *format, ...);
 int				ft_putchar_to_buffer(t_printf *s, char c);
 int				ft_convert(t_printf *s);
+
 void			ft_get_flags(t_printf *s);
 int				ft_get_width(t_printf *s);
 int				ft_get_precision(t_printf *s);
 void			ft_get_size(t_printf *s);
 void			ft_get_size2(t_printf *s, const char *size);
+
 int				ft_printf_c(t_printf *s);
 int				ft_type_c(t_printf *s);
+
 int				ft_printf_str(t_printf *s);
 int				ft_printf_wstr(t_printf *s);
 int				ft_type_str(t_printf *s);
+
 int				ft_printf_ullint(t_printf *s);
 int				ft_printf_ullint2(t_printf *s, char *number);
 int				ft_type_uint(t_printf *s);
 int				ft_type_uint2(t_printf *s);
+
 int				ft_printf_llint(t_printf *s);
 int				ft_type_int(t_printf *s);
+
 void			ft_type_n(t_printf *s);
 void			ft_type_n2(t_printf *s);
+
 int				ft_type_unknown(t_printf *s);
 
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
