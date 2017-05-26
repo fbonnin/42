a315b0d81a897309f0359995d891dcb433fe38a7
Florent BONNIN <florent-bonnin@live.fr>
2017-05-26 18:48:04 +0200

    checker

---
+++ libftprintf/main.c
@@ -0,0 +1,9 @@
+#include "libft.h"
+
+int main()
+{
+	ft_printf("aa%%bb");
+	ft_putendl("");
+	printf("aa%%bb");
+	return (0);
+}
---
+++ libftprintf/simple/simple.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   simple.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbonnin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/26 14:39:02 by fbonnin           #+#    #+#             */
+/*   Updated: 2017/05/26 14:39:03 by fbonnin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SIMPLE_H
+# define SIMPLE_H
+
+char	*ft_ullint_to_string(unsigned long long int n,
+			unsigned long long int base, int uppercase);
+
+#endif
