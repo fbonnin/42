b08650afac83efc2313cefb9d214d13fecc90da3
Florent Bonnin <florent-bonnin@live.fr>
2017-06-02 15:53:31 +0200

    rmold

--- libft/main.c
+++
@@ -1,7 +0,0 @@
-#include "libft.h"
-
-int	main()
-{
-	ft_printf("%s\n", "string");
-	return (0);
-}
--- libft/simple/simple.h
+++
@@ -1,7 +0,0 @@
-#ifndef SIMPLE_H
-# define SIMPLE_H
-
-char	*ft_ullint_to_string(unsigned long long int n,
-			unsigned long long int base, int uppercase);
-
-#endif
