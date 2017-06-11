b34623cf14c4460de933d946421bfdbb066091fe
Florent Bonnin <florent-bonnin@live.fr>
2017-06-11 03:05:56 +0200

    bubble

--- push_swap/push_swap_sources/bubble_sort.c
+++ push_swap/push_swap_sources/bubble_sort.c
@@ -12,22 +12,32 @@
 
 #include "push_swap.h"
 
+static int	sorted(t_push_swap *s)
+{
+	int i;
+
+	i = 0;
+	while (i < s->nb_numbers - 1)
+	{
+		if (s->numbers2[i] > s->numbers2[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	bubble_sort(t_push_swap *s, int save)
 {
-	int sorted;
 	int i;
 	int t;
 
-	sorted = 0;
-	while (!sorted)
+	while (!sorted(s))
 	{
-		sorted = 1;
 		i = 0;
 		while (i < s->nb_numbers - 1)
 		{
 			if (s->numbers2[i] > s->numbers2[i + 1])
 			{
-				sorted = 0;
 				t = s->numbers2[i];
 				s->numbers2[i] = s->numbers2[i + 1];
 				s->numbers2[i + 1] = t;
