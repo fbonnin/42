4a85e3e2d2f344cf1f12956b56c0dfd22633f9e2
Florent Bonnin <florent-bonnin@live.fr>
2017-07-20 00:08:47 +0200

    m

--- lem-in/main.c
+++ lem-in/main.c
@@ -7,20 +7,26 @@ int		main(void)
 	s.moves = NULL;
 	if (input(&s) == -1 || find_moves(&s) == -1)
 		return (0);
+	s.moves = list_moves_to_moves(&s.list_moves);
+	sort_moves(s.moves, s.list_moves.nb_moves);
+	print_lines(s.lines, s.nb_lines);
+	print_moves(s.moves, s.list_moves.nb_moves);
+	free_all(&s.lines, &s.nodes, s.nb_nodes_expected, s.moves);
+	return (0);
 }
 
 int		input(t_s *s)
 {
-	s->lines->first = NULL;
-	s->lines->last = NULL;
+	s->lines.first = NULL;
+	s->lines.last = NULL;
 	get_input(&s->lines, &s->nb_nodes_expected);
 	s->nodes = alloc_nodes(s->nb_nodes_expected);
 	parse_lines(s);
 	if (s->nb_ants < 0 ||
-	s->nb_nodes[0].name == NULL || s->nb_nodes[1].name == NULL)
+	s->nodes[0].name == NULL || s->nodes[1].name == NULL)
 	{
 		ft_printf("ERROR\n");
-		free_all(&s->lines, &s->nodes, s->nb_nodes, s->moves);
+		free_all(&s->lines, &s->nodes, s->nb_nodes_expected, s->moves);
 		return (-1);
 	}
 	return (0);
