2913ecc8eb24b2db914a0e430d492c5b607d7e4a
Florent Bonnin <florent-bonnin@live.fr>
2018-02-03 17:10:35 +0100

    m

---
+++ quant/parse_pdf.c
@@ -0,0 +1,29 @@
+int	parse_pdf(char *input, char *output, char *lien)
+{
+	char			*text;
+	t_notification	notification;
+	int				r;
+
+	text = NULL;
+	init_notification(notification);
+	if (pdf_to_text(input) == -1)
+		return end(text, &notification, "pdf to text\n");
+	text = load_file(input);
+	remove(input);
+	if (text == NULL)
+		return end(text, &notification, "load text\n");
+	if (extract_data(text, &notification) == -1)
+		return end(text, &notification, "extract data\n");
+	if (write_notification(&notification, output) == -1)
+		return end(text, &notification, "write notification\n");
+	end(text, &notification, "");
+	return 0;
+}
+
+int	end(char *text, t_notification *notification, char *s)
+{
+	free_notification(notification);
+	free(text);
+	printf("%s", s);
+	return -1;	
+}
