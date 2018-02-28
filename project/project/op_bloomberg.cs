f4656f7da423d4ad89d5b61906d94ba0d526e6e1
unknown <fbonnin>
2018-02-28 17:05:12 +0100

    m

--- project/project/op_bloomberg.cs
+++ project/project/op_bloomberg.cs
@@ -8,51 +8,6 @@ using Bloomberglp.Blpapi;
 
 namespace project
 {
-    class OP_BLOOMBERG : OPERATION
-    {
-        public void Make_request()
-        {
-            session.SendRequest(request, null);
-            bool done = false;
-            while (!done)
-            {
-                Event bloomberg_event = session.NextEvent();
-                if (bloomberg_event == Event.EventType.RESPONSE)
-                {
-
-                }
-            }
-        }
-    }
-
-    class BLOOM_HISTORICAL_DATA_REQUEST : OPERATION
-    {
-        Request request;
-        string[] fields;
-        string[] tickers;
-        string start_date;
-        string end_date;
-
-        public BLOOM_HISTORICAL_DATA_REQUEST(SOURCE source, DATABASE database, string table, string[] fields, string[] tickers, string start_date, string end_date) : base(source, database, table)
-        {
-            this.fields = fields;
-            this.tickers = tickers;
-            this.start_date = start_date;
-            this.end_date = end_date;
-        }
-
-        private void Make_request()
-        {
-            request = source.service.CreateRequest("Historical Data Request");
-            foreach (string field in fields)
-                request.Append("fields", field);
-            foreach (string ticker in tickers)
-                request.Append("securities", ticker);
-            request.Set("startDate", start_date);
-            request.Set("endDate", end_date);
-        }
-    }
-
     class BLOOM_REQUEST1
     {
         SRC_BLOOM source;
@@ -60,8 +15,9 @@ namespace project
         string ticker;
         DateTime start_date;
         DateTime end_date;
+        Request request;
 
-        public BLOOM_REQUEST1(SRC_BLOOMBERG source, string[] fields, string ticker, string start_date, string end_date)
+        public BLOOM_REQUEST1(SRC_BLOOM source, string[] fields, string ticker, DateTime start_date, DateTime end_date)
         {
             this.source = source;
             this.fields = fields;
@@ -70,14 +26,50 @@ namespace project
             this.end_date = end_date;
         }
 
-        private Request Make_request()
+        private Request Prepare_request()
         {
-            request = source.service.CreateRequest("Historical Data Request");
+            request = source.Create_request("Historical Data Request");
             foreach (string field in fields)
                 request.Append("fields", field);
             request.Append("securities", ticker);
             request.Set("startDate", start_date);
             request.Set("endDate", end_date);
+            return request;
+        }
+
+        private Object[][] Make_request()
+        {
+            List<Object[]> result = new List<Object[]>();
+
+            source.Send_request(request);
+            while (true)
+            {
+                Event event_bloom = source.Next_event();
+                foreach (Message message in event_bloom)
+                {
+                    if (event_bloom.Type != Event.EventType.RESPONSE &&
+                        event_bloom.Type != Event.EventType.PARTIAL_RESPONSE)
+                        continue;
+                    Element security_data = message.GetElement("securityData");
+                    Element field_datas = security_data.GetElement("fieldData[]");
+                    for (int i = 0; i < field_datas.NumValues; i++)
+                    {
+                        result.Add(new Object[fields.Length]);
+                        Element field_data = field_datas.GetValueAsElement(i);
+                        for (int j = 0; j < fields.Length; j++)
+                        {
+                            result.Last()[j] = field_data.GetElementAsString(fields[j]);
+                            Object value;
+                            if (fields[j] == "ticker")
+                                value = String.Copy(ticker);
+                            else
+                                value = field_data.GetElementAsString(fields[j]);
+                        }
+                    }
+                }
+                if (event_bloom.Type == Event.EventType.RESPONSE)
+                    break;
+            }
         }
     }
 }
--- project/project/src_bloomberg.cs
+++ project/project/src_bloomberg.cs
@@ -14,8 +14,8 @@ namespace project
 
     class SRC_BLOOM : SOURCE
     {
-        Session session;
-        Service service;
+        protected Session session;
+        protected Service service;
 
         public SRC_BLOOM()
         {
@@ -30,6 +30,26 @@ namespace project
         ~SRC_BLOOMBERG()
         {
             session.Stop();
+        }
+
+        public Request Create_request(string type)
+        {
+            return service.CreateRequest(type);
+        }
+
+        public void Send_request(Request request)
+        {
+            session.SendRequest(request, null);
+        }
+
+        public Event Next_event()
+        {
+            return session.NextEvent();
+        }
+
+        public override Object[][] Request1(string[] fields, string ticker, DateTime start_date, DateTime end_date)
+        {
+            BLOOM_REQUEST1 request1 = new BLOOM_REQUEST1(this, fields, ticker, start_date, end_date);
         }
     }
 }
