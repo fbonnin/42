b4ae570cdae2ad5d0813e3bcf7c8fc5d2228c684
Florent BONNIN <fbonnin@e1r11p8.42.fr>
2019-01-09 14:02:43 +0100

    m

---
+++ piscine/day01/ssap.php
@@ -0,0 +1,56 @@
+#!/usr/bin/php
+
+<?PHP
+
+function ft_split($s)
+{
+	$nb_words = 0;
+	$mode = 0;
+	for ($i = 0; $i < strlen($s); $i++)
+	{
+		if ($s[$i] != ' ')
+		{
+			if ($mode == 0)
+			{
+				$word = $s[$i];
+			}
+			else
+			{
+				$word = $word . $s[$i];
+			}
+			$mode = 1;
+		}
+		else
+		{
+			if ($mode == 1)
+			{
+				$result[$nb_words] = $word;
+				$nb_words++;
+			}
+			$mode = 0;
+		}
+	}
+	if ($mode == 1)
+	{
+		$result[$nb_words] = $word;
+	}
+	sort($result);
+	return $result;
+}
+
+$total = "";
+
+for ($i = 1; $i < count($argv); $i++)
+{
+	$total = $total . $argv[$i] . " ";
+}
+
+
+$array = ft_split($total);
+
+for ($i = 0; $i < count($array); $i++)
+{
+	echo "$array[$i]\n";
+}
+
+?>
