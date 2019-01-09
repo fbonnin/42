db8b69e85a1f19c236160c1bdb7b848455004189
Florent BONNIN <fbonnin@e1r11p8.42.fr>
2019-01-09 13:52:36 +0100

    m

---
+++ piscine/day01/epur_str.php
@@ -0,0 +1,50 @@
+#!/usr/bin/php
+
+<?PHP
+
+function ft_split($s)
+{
+	$nb_words = 0;
+	$mode = 0;
+	for ($i = 0; $i < strlen($s); $i++)
+	{
+		if ($s[$i] != ' ')
+		{
+			if ($mode == 0)
+			{
+				$word = $s[$i];
+			}
+			else
+			{
+				$word = $word . $s[$i];
+			}
+			$mode = 1;
+		}
+		else
+		{
+			if ($mode == 1)
+			{
+				$result[$nb_words] = $word;
+				$nb_words++;
+			}
+			$mode = 0;
+		}
+	}
+	if ($mode == 1)
+	{
+		$result[$nb_words] = $word;
+	}
+	return $result;
+}
+
+$array = ft_split($argv[1]);
+$c = count($array);
+
+for ($i = 0; $i < count($array); $i++)
+{
+	echo "$array[$i]";
+	if ($i < count($array) - 1)
+		echo " ";
+}
+
+?>
